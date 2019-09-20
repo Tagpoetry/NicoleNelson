ee987a49014fbfd1b9517d62f93d993308365688
nicole nelson <tagpoetry@gmail.com>
2019-09-19 21:13:55 -0500

    Scrpts Js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
-const msg= prompt("What is your name?");
-console.info(msg);
+function greeter (question = " what is your name?") {
+ return prompt (question = " Nicole Nelson");
+
+}
+
+
+const answer = greeter("who is your favorite wrestler?");
+console.log(answer);
 
