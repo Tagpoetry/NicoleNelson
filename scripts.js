f552fec6ea14ff6409efe044e20d97a00c30f308
nicole nelson <tagpoetry@gmail.com>
2019-09-24 21:15:01 -0500

    Index syntax changes

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@
 const hamburger = document.querySelector(".fa-hamburger");
 const hiddenUL = document.querySelector ("ul");
 
+hamburger.addEventListener('click', function () {
+  // hiddenUL classlist remove ("is-hidden--mobile");
+  hiddenUL.classList.toggle("is-hidden--mobile");
+});
 
 function greeter (question = " what is your name?") {
   const answer =prompt(question);
