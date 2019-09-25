85c3bc5d5f3100d95bb851a70deb3b9ac866ee2c
nicole nelson <tagpoetry@gmail.com>
2019-09-24 21:23:59 -0500

    Hamburger function click

--- scripts.js
+++ scripts.js
@@ -8,23 +8,3 @@ hamburger.addEventListener('click', function () {
   // hiddenUL classlist remove ("is-hidden--mobile");
   hiddenUL.classList.toggle("is-hidden--mobile");
 });
-
-function greeter (question = " what is your name?") {
-  const answer =prompt(question);
- return prompt (question);
- if (answer==="") {
-
- }
-
-}
-
-
-return answer;
-document.querySelector('h1').textContent=greeter();
-
-
-// const answer =greeter();
-// document.querySelector("#root").innerHTML = '<p>${greeter()}</p>
-
-console.log(answer);
-
