e0fda2130b79317c656434b67649c5e8012ca658
nicole nelson <tagpoetry@gmail.com>
2019-09-24 20:18:33 -0500

    root syntax change in html

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 function greeter (question = " what is your name?") {
- return prompt (question = " Nicole Nelson");
+  const answer =prompt(question);
+ return prompt (question);
+ if (answer==="") {
+
+ }
 
 }
 
 
-const answer = greeter("who is your favorite wrestler?");
+return answer;
+document.querySelector('h1').textContent=greeter();
+
+
+// const answer =greeter();
+// document querySelector("h1").textContext=answer
+
 console.log(answer);
 
